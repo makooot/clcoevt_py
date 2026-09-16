7817c844b361fffc06dbce352cf1da7bfaf7e7c6
HASHIGAYA, Makoto <6903330+makooot@users.noreply.github.com>
2026-09-16 23:36:43 +0900

    refactor: simplify import statements for clarity

--- src/clcoevt/__init__.py
+++ src/clcoevt/__init__.py
@@ -1,7 +1,6 @@
-from .core import Clcoevt
-from .types import (
-    ClcoevtCommandDetail,
-    ClcoevtShowHelpException,
-    ClcoevtShowVersionException,
-    ClcoevtValueError,
-)
+from .core import Clcoevt as Clcoevt
+from .types import ClcoevtCommandDetail as ClcoevtCommandDetail
+from .types import ClcoevtShowHelpException as ClcoevtShowHelpException
+from .types import ClcoevtShowVersionException as ClcoevtShowVersionException
+from .types import ClcoevtValueError as ClcoevtValueError
+
